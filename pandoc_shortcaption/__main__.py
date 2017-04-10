0e775a564a48eed811ec1f0e3dc3eb642f67b5e6
Michael Ganger <mganger747@gmail.com>
2017-04-10 14:31:54 -0400

    Added feature to make short captions for tables

--- pandoc_shortcaption/__main__.py
+++ pandoc_shortcaption/__main__.py
@@ -1,6 +1,6 @@
 #!/bin/env /home/eisamar/.virtualenvs/pandoc-shortcaption/bin/python
 
-from pandocfilters import toJSONFilter, RawInline
+from pandocfilters import toJSONFilter, RawInline, RawBlock
 import logging
 FORMAT = "%(asctime)s %(levelname)-8s%(module)-10s %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.ERROR)
@@ -15,6 +15,18 @@ def remove_prefix(text, prefix):
     if text.startswith(prefix):
         return text[len(prefix):]
     return text  # or whatever
+
+def text(c):
+    if type(c) == list:
+        return ' '.join(text(d) for d in c)
+    elif c['t'] == 'Str':
+        return c['c'].replace('{','\\{').replace('}','\\}')
+    elif c['t'] == 'RawInline':
+        return c['c'][1]
+    elif c['t'] == 'Math':
+        return '$'+c['c'][1]+'$'
+    else:
+        return ' '.join(text(d) for d in c['c'])
 
 def shortcap(key, value, format, meta):
 
@@ -52,6 +64,22 @@ def shortcap(key, value, format, meta):
 
             return RawInline('tex', raw)
 
+    if key == "Table" and format == "latex":
+        caption = text(value[0])
+        short = caption.split('.')[0]+'.'
+        raw = "\\begin{longtable}[]{@{}ll@{}}\n"
+        raw += "\\caption[{}]{{{}}}".format(short,caption)
+        raw += '\\tabularnewline\n \\toprule\n'
+        raw += ' & '.join(text(l) for l in value[3])
+        raw += "\\tabularnewline\n \\midrule \\endfirsthead \\toprule\n"
+        raw += ' & '.join(text(l) for l in value[3])
+        raw += "\\tabularnewline\n \\midrule \\endhead\n"
+        for line in value[4]:
+            raw += ' & '.join(text(column) for column in line)
+            raw += '\\tabularnewline\n'
+        raw += '\\bottomrule \\end{longtable}'
+        
+        return RawBlock('tex',raw)
 
 def main():
     toJSONFilter(shortcap)
